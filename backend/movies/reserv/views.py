1f852bcbe3c4be7e3173f5428f1a0556e5a35582
Klaci00 <kenyolaszlo6@gmail.com>
2024-09-20 17:31:27 +0200

    got rid of showdetailview update function, since it became obsolete

--- backend/movies/reserv/views.py
+++ backend/movies/reserv/views.py
@@ -17,27 +17,7 @@ from .myFunctions import myFunctions
 class ShowDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset=Show.objects.all()
     serializer_class=ShowSerializer
-    def partial_update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        data = request.data
-                # Fetch the CustomUser instance
-        try:
-            user = CustomUser.objects.get(id=data['user_id'])
-        except CustomUser.DoesNotExist:
-            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-
-        print(instance.seat_a)
-        print(instance.seat_b)
-        # Update seat values from 1 to 2
-        if 'seat_a' in data and data['seat_a'] == 1:
-            data['seat_a'] = 2
-        if 'seat_b' in data and data['seat_b'] == 1:
-            data['seat_b'] = 2
-        serializer = self.get_serializer(instance, data=data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    
     
 class VenueDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset=Venue.objects.all()
