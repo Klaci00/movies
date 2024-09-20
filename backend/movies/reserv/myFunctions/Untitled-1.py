8125e9c05b774d7bb4f618b388fc47b19dfa9520
Klaci00 <kenyolaszlo6@gmail.com>
2024-09-20 20:26:29 +0200

    appended django myFunctions module

--- backend/movies/reserv/myFunctions/Untitled-1.py
+++ backend/movies/reserv/myFunctions/Untitled-1.py
@@ -1,8 +1,8 @@
 
 for i in range(1,181):
     if i<10:
-        print("'seat_00"+str(i)+"': data['seat_00"+str(i) +"'],")
+        print("'seat_00"+str(i)+"': venue.seat_00"+str(i)+",")
     elif i<100:
-        print("'seat_0"+str(i)+"': data['seat_0"+str(i) +"'],")
+        print("'seat_0"+str(i)+"': venue.seat_0"+str(i)+",")
     else: 
-        print("'seat_"+str(i)+"': data['seat_"+str(i) +"'],")
+        print("'seat_"+str(i)+"': venue.seat_"+str(i)+",")
