0313dd877a8f6ae9fd7b612fdf96b287b4ec4121
Klaci00 <kenyolaszlo6@gmail.com>
2024-09-22 14:52:57 +0200

    setting up transition to production backend model

--- backend/movies/reserv/myFunctions/Untitled-1.py
+++ backend/movies/reserv/myFunctions/Untitled-1.py
@@ -1,71 +1,29 @@
-def seatMaker():
-    seatList=[]
-    for i in range(1,181):
-        if i<10: seatList.append(f'seat_00{i}')
-        elif i<100: seatList.append(f'seat_0{i}')
-        else: seatList.append(f'seat_{i}')
-    return seatList
+'''mystring=""
+for i in range(1,181):
+    if i<10:
+        mystring+=f"const [seat00{i}, setSeat00{i}] = useState(0);\n"
+    elif i<100:
+        mystring+=f"const [seat0{i}, setSeat0{i}] = useState(0);\n"
+    else:
+        mystring+=f"const [seat{i}, setSeat{i}] = useState(0);\n"
 
-human={'name': "Adam", 'height': 184}
-keyword='height'
-print(human[f'{keyword}'])
-print(seatMaker())
-'''def dataMocker():
-    data={}
-    for i in range(1,181):
-        if i<10:
-            data.update({f'seat_00{str(i)}': i,})
-        elif i<100:
-            data.update({f'seat_0{str(i)}': i,})        
-        else: 
-            data.update({f'seat_{str(i)}': i,})    
-    return data
-def seatMaker():
-    seatDictList=[]
-    for i in range(1,181):
-        if i<10:
-            seatDictList.append("{'seat_00"+str(i)+"': data['seat_00"+str(i)+"'],}")
-        elif i<100:
-            seatDictList.append("{seat_0"+str(i)+"': data['seat_0"+str(i)+"'],}")
-        else: 
-            seatDictList.append("{'seat_"+str(i)+"': data['seat_"+str(i)+"'],}")
-
-    return seatDictList
-data={'title': 'Titanic',
-            'room_name': 'Nagyterem',
-            'showtime': 'xy',}
-data.update(dataMocker())
-def reservData3(user,data):
-    dict1={
-            'owner': user,
-            'title': data['title'],
-            'room_name': data['room_name'],
-            'showtime': data['showtime'],}
-        
-    dict1.update(data)
-    print(dict1)
-    return''
-
-reservData3(user=1,data=data)
-''''''
-data={}
-data.update(dataMocker())
-
+print(mystring)
 '''
+mystring=""
+'''for i in range(1,181):
+    if i<10:
+        mystring+=f"seat00{i}, setSeat00{i},\n"
+    elif i<100:
+        mystring+=f"seat0{i}, setSeat0{i},\n"
+    else:
+        mystring+=f"seat{i}, setSeat{i},\n"
 '''
-dict1={
-    'owner': 'user',
-    'title': 'Titanic,'
-}
-dict2={
-    'seat1': 'seat1',
-}
+for i in range(1,181):
+    if i<10:
+        mystring+=f"seat00{i}, setSeat00{i},"
+    elif i<100:
+        mystring+=f"seat0{i}, setSeat0{i},"
+    else:
+        mystring+=f"seat{i}, setSeat{i},"
 
-dict3={
-    'seat_count': 'seat_count'
-}
-
-complete = dict1 | dict2 | dict3
-
-print(complete)
-'''
+print(mystring)
---
+++ frontend/movies-front/src/Shows/SetseatStates.jsx
@@ -0,0 +1,374 @@
+import { useState } from 'react';
+
+export const useSeatStates = () => {
+  const [seatA, setSeatA] = useState(0);
+  const [seatB, setSeatB] = useState(0);
+  const [seatC, setSeatC] = useState(0);
+  const [seatD, setSeatD] = useState(0);
+  const [seat001, setSeat001] = useState(0);
+  const [seat002, setSeat002] = useState(0);
+  const [seat003, setSeat003] = useState(0);
+  const [seat004, setSeat004] = useState(0);
+  const [seat005, setSeat005] = useState(0);
+  const [seat006, setSeat006] = useState(0);
+  const [seat007, setSeat007] = useState(0);
+  const [seat008, setSeat008] = useState(0);
+  const [seat009, setSeat009] = useState(0);
+  const [seat010, setSeat010] = useState(0);
+  const [seat011, setSeat011] = useState(0);
+  const [seat012, setSeat012] = useState(0);
+  const [seat013, setSeat013] = useState(0);
+  const [seat014, setSeat014] = useState(0);
+  const [seat015, setSeat015] = useState(0);
+  const [seat016, setSeat016] = useState(0);
+  const [seat017, setSeat017] = useState(0);
+  const [seat018, setSeat018] = useState(0);
+  const [seat019, setSeat019] = useState(0);
+  const [seat020, setSeat020] = useState(0);
+  const [seat021, setSeat021] = useState(0);
+  const [seat022, setSeat022] = useState(0);
+  const [seat023, setSeat023] = useState(0);
+  const [seat024, setSeat024] = useState(0);
+  const [seat025, setSeat025] = useState(0);
+  const [seat026, setSeat026] = useState(0);
+  const [seat027, setSeat027] = useState(0);
+  const [seat028, setSeat028] = useState(0);
+  const [seat029, setSeat029] = useState(0);
+  const [seat030, setSeat030] = useState(0);
+  const [seat031, setSeat031] = useState(0);
+  const [seat032, setSeat032] = useState(0);
+  const [seat033, setSeat033] = useState(0);
+  const [seat034, setSeat034] = useState(0);
+  const [seat035, setSeat035] = useState(0);
+  const [seat036, setSeat036] = useState(0);
+  const [seat037, setSeat037] = useState(0);
+  const [seat038, setSeat038] = useState(0);
+  const [seat039, setSeat039] = useState(0);
+  const [seat040, setSeat040] = useState(0);
+  const [seat041, setSeat041] = useState(0);
+  const [seat042, setSeat042] = useState(0);
+  const [seat043, setSeat043] = useState(0);
+  const [seat044, setSeat044] = useState(0);
+  const [seat045, setSeat045] = useState(0);
+  const [seat046, setSeat046] = useState(0);
+  const [seat047, setSeat047] = useState(0);
+  const [seat048, setSeat048] = useState(0);
+  const [seat049, setSeat049] = useState(0);
+  const [seat050, setSeat050] = useState(0);
+  const [seat051, setSeat051] = useState(0);
+  const [seat052, setSeat052] = useState(0);
+  const [seat053, setSeat053] = useState(0);
+  const [seat054, setSeat054] = useState(0);
+  const [seat055, setSeat055] = useState(0);
+  const [seat056, setSeat056] = useState(0);
+  const [seat057, setSeat057] = useState(0);
+  const [seat058, setSeat058] = useState(0);
+  const [seat059, setSeat059] = useState(0);
+  const [seat060, setSeat060] = useState(0);
+  const [seat061, setSeat061] = useState(0);
+  const [seat062, setSeat062] = useState(0);
+  const [seat063, setSeat063] = useState(0);
+  const [seat064, setSeat064] = useState(0);
+  const [seat065, setSeat065] = useState(0);
+  const [seat066, setSeat066] = useState(0);
+  const [seat067, setSeat067] = useState(0);
+  const [seat068, setSeat068] = useState(0);
+  const [seat069, setSeat069] = useState(0);
+  const [seat070, setSeat070] = useState(0);
+  const [seat071, setSeat071] = useState(0);
+  const [seat072, setSeat072] = useState(0);
+  const [seat073, setSeat073] = useState(0);
+  const [seat074, setSeat074] = useState(0);
+  const [seat075, setSeat075] = useState(0);
+  const [seat076, setSeat076] = useState(0);
+  const [seat077, setSeat077] = useState(0);
+  const [seat078, setSeat078] = useState(0);
+  const [seat079, setSeat079] = useState(0);
+  const [seat080, setSeat080] = useState(0);
+  const [seat081, setSeat081] = useState(0);
+  const [seat082, setSeat082] = useState(0);
+  const [seat083, setSeat083] = useState(0);
+  const [seat084, setSeat084] = useState(0);
+  const [seat085, setSeat085] = useState(0);
+  const [seat086, setSeat086] = useState(0);
+  const [seat087, setSeat087] = useState(0);
+  const [seat088, setSeat088] = useState(0);
+  const [seat089, setSeat089] = useState(0);
+  const [seat090, setSeat090] = useState(0);
+  const [seat091, setSeat091] = useState(0);
+  const [seat092, setSeat092] = useState(0);
+  const [seat093, setSeat093] = useState(0);
+  const [seat094, setSeat094] = useState(0);
+  const [seat095, setSeat095] = useState(0);
+  const [seat096, setSeat096] = useState(0);
+  const [seat097, setSeat097] = useState(0);
+  const [seat098, setSeat098] = useState(0);
+  const [seat099, setSeat099] = useState(0);
+  const [seat100, setSeat100] = useState(0);
+  const [seat101, setSeat101] = useState(0);
+  const [seat102, setSeat102] = useState(0);
+  const [seat103, setSeat103] = useState(0);
+  const [seat104, setSeat104] = useState(0);
+  const [seat105, setSeat105] = useState(0);
+  const [seat106, setSeat106] = useState(0);
+  const [seat107, setSeat107] = useState(0);
+  const [seat108, setSeat108] = useState(0);
+  const [seat109, setSeat109] = useState(0);
+  const [seat110, setSeat110] = useState(0);
+  const [seat111, setSeat111] = useState(0);
+  const [seat112, setSeat112] = useState(0);
+  const [seat113, setSeat113] = useState(0);
+  const [seat114, setSeat114] = useState(0);
+  const [seat115, setSeat115] = useState(0);
+  const [seat116, setSeat116] = useState(0);
+  const [seat117, setSeat117] = useState(0);
+  const [seat118, setSeat118] = useState(0);
+  const [seat119, setSeat119] = useState(0);
+  const [seat120, setSeat120] = useState(0);
+  const [seat121, setSeat121] = useState(0);
+  const [seat122, setSeat122] = useState(0);
+  const [seat123, setSeat123] = useState(0);
+  const [seat124, setSeat124] = useState(0);
+  const [seat125, setSeat125] = useState(0);
+  const [seat126, setSeat126] = useState(0);
+  const [seat127, setSeat127] = useState(0);
+  const [seat128, setSeat128] = useState(0);
+  const [seat129, setSeat129] = useState(0);
+  const [seat130, setSeat130] = useState(0);
+  const [seat131, setSeat131] = useState(0);
+  const [seat132, setSeat132] = useState(0);
+  const [seat133, setSeat133] = useState(0);
+  const [seat134, setSeat134] = useState(0);
+  const [seat135, setSeat135] = useState(0);
+  const [seat136, setSeat136] = useState(0);
+  const [seat137, setSeat137] = useState(0);
+  const [seat138, setSeat138] = useState(0);
+  const [seat139, setSeat139] = useState(0);
+  const [seat140, setSeat140] = useState(0);
+  const [seat141, setSeat141] = useState(0);
+  const [seat142, setSeat142] = useState(0);
+  const [seat143, setSeat143] = useState(0);
+  const [seat144, setSeat144] = useState(0);
+  const [seat145, setSeat145] = useState(0);
+  const [seat146, setSeat146] = useState(0);
+  const [seat147, setSeat147] = useState(0);
+  const [seat148, setSeat148] = useState(0);
+  const [seat149, setSeat149] = useState(0);
+  const [seat150, setSeat150] = useState(0);
+  const [seat151, setSeat151] = useState(0);
+  const [seat152, setSeat152] = useState(0);
+  const [seat153, setSeat153] = useState(0);
+  const [seat154, setSeat154] = useState(0);
+  const [seat155, setSeat155] = useState(0);
+  const [seat156, setSeat156] = useState(0);
+  const [seat157, setSeat157] = useState(0);
+  const [seat158, setSeat158] = useState(0);
+  const [seat159, setSeat159] = useState(0);
+  const [seat160, setSeat160] = useState(0);
+  const [seat161, setSeat161] = useState(0);
+  const [seat162, setSeat162] = useState(0);
+  const [seat163, setSeat163] = useState(0);
+  const [seat164, setSeat164] = useState(0);
+  const [seat165, setSeat165] = useState(0);
+  const [seat166, setSeat166] = useState(0);
+  const [seat167, setSeat167] = useState(0);
+  const [seat168, setSeat168] = useState(0);
+  const [seat169, setSeat169] = useState(0);
+  const [seat170, setSeat170] = useState(0);
+  const [seat171, setSeat171] = useState(0);
+  const [seat172, setSeat172] = useState(0);
+  const [seat173, setSeat173] = useState(0);
+  const [seat174, setSeat174] = useState(0);
+  const [seat175, setSeat175] = useState(0);
+  const [seat176, setSeat176] = useState(0);
+  const [seat177, setSeat177] = useState(0);
+  const [seat178, setSeat178] = useState(0);
+  const [seat179, setSeat179] = useState(0);
+  const [seat180, setSeat180] = useState(0);
+  return {
+    seatA, setSeatA,
+    seatB, setSeatB,
+    seatC, setSeatC,
+    seatD, setSeatD,
+    seat001, setSeat001,
+    seat002, setSeat002,
+    seat003, setSeat003,
+    seat004, setSeat004,
+    seat005, setSeat005,
+    seat006, setSeat006,
+    seat007, setSeat007,
+    seat008, setSeat008,
+    seat009, setSeat009,
+    seat010, setSeat010,
+    seat011, setSeat011,
+    seat012, setSeat012,
+    seat013, setSeat013,
+    seat014, setSeat014,
+    seat015, setSeat015,
+    seat016, setSeat016,
+    seat017, setSeat017,
+    seat018, setSeat018,
+    seat019, setSeat019,
+    seat020, setSeat020,
+    seat021, setSeat021,
+    seat022, setSeat022,
+    seat023, setSeat023,
+    seat024, setSeat024,
+    seat025, setSeat025,
+    seat026, setSeat026,
+    seat027, setSeat027,
+    seat028, setSeat028,
+    seat029, setSeat029,
+    seat030, setSeat030,
+    seat031, setSeat031,
+    seat032, setSeat032,
+    seat033, setSeat033,
+    seat034, setSeat034,
+    seat035, setSeat035,
+    seat036, setSeat036,
+    seat037, setSeat037,
+    seat038, setSeat038,
+    seat039, setSeat039,
+    seat040, setSeat040,
+    seat041, setSeat041,
+    seat042, setSeat042,
+    seat043, setSeat043,
+    seat044, setSeat044,
+    seat045, setSeat045,
+    seat046, setSeat046,
+    seat047, setSeat047,
+    seat048, setSeat048,
+    seat049, setSeat049,
+    seat050, setSeat050,
+    seat051, setSeat051,
+    seat052, setSeat052,
+    seat053, setSeat053,
+    seat054, setSeat054,
+    seat055, setSeat055,
+    seat056, setSeat056,
+    seat057, setSeat057,
+    seat058, setSeat058,
+    seat059, setSeat059,
+    seat060, setSeat060,
+    seat061, setSeat061,
+    seat062, setSeat062,
+    seat063, setSeat063,
+    seat064, setSeat064,
+    seat065, setSeat065,
+    seat066, setSeat066,
+    seat067, setSeat067,
+    seat068, setSeat068,
+    seat069, setSeat069,
+    seat070, setSeat070,
+    seat071, setSeat071,
+    seat072, setSeat072,
+    seat073, setSeat073,
+    seat074, setSeat074,
+    seat075, setSeat075,
+    seat076, setSeat076,
+    seat077, setSeat077,
+    seat078, setSeat078,
+    seat079, setSeat079,
+    seat080, setSeat080,
+    seat081, setSeat081,
+    seat082, setSeat082,
+    seat083, setSeat083,
+    seat084, setSeat084,
+    seat085, setSeat085,
+    seat086, setSeat086,
+    seat087, setSeat087,
+    seat088, setSeat088,
+    seat089, setSeat089,
+    seat090, setSeat090,
+    seat091, setSeat091,
+    seat092, setSeat092,
+    seat093, setSeat093,
+    seat094, setSeat094,
+    seat095, setSeat095,
+    seat096, setSeat096,
+    seat097, setSeat097,
+    seat098, setSeat098,
+    seat099, setSeat099,
+    seat100, setSeat100,
+    seat101, setSeat101,
+    seat102, setSeat102,
+    seat103, setSeat103,
+    seat104, setSeat104,
+    seat105, setSeat105,
+    seat106, setSeat106,
+    seat107, setSeat107,
+    seat108, setSeat108,
+    seat109, setSeat109,
+    seat110, setSeat110,
+    seat111, setSeat111,
+    seat112, setSeat112,
+    seat113, setSeat113,
+    seat114, setSeat114,
+    seat115, setSeat115,
+    seat116, setSeat116,
+    seat117, setSeat117,
+    seat118, setSeat118,
+    seat119, setSeat119,
+    seat120, setSeat120,
+    seat121, setSeat121,
+    seat122, setSeat122,
+    seat123, setSeat123,
+    seat124, setSeat124,
+    seat125, setSeat125,
+    seat126, setSeat126,
+    seat127, setSeat127,
+    seat128, setSeat128,
+    seat129, setSeat129,
+    seat130, setSeat130,
+    seat131, setSeat131,
+    seat132, setSeat132,
+    seat133, setSeat133,
+    seat134, setSeat134,
+    seat135, setSeat135,
+    seat136, setSeat136,
+    seat137, setSeat137,
+    seat138, setSeat138,
+    seat139, setSeat139,
+    seat140, setSeat140,
+    seat141, setSeat141,
+    seat142, setSeat142,
+    seat143, setSeat143,
+    seat144, setSeat144,
+    seat145, setSeat145,
+    seat146, setSeat146,
+    seat147, setSeat147,
+    seat148, setSeat148,
+    seat149, setSeat149,
+    seat150, setSeat150,
+    seat151, setSeat151,
+    seat152, setSeat152,
+    seat153, setSeat153,
+    seat154, setSeat154,
+    seat155, setSeat155,
+    seat156, setSeat156,
+    seat157, setSeat157,
+    seat158, setSeat158,
+    seat159, setSeat159,
+    seat160, setSeat160,
+    seat161, setSeat161,
+    seat162, setSeat162,
+    seat163, setSeat163,
+    seat164, setSeat164,
+    seat165, setSeat165,
+    seat166, setSeat166,
+    seat167, setSeat167,
+    seat168, setSeat168,
+    seat169, setSeat169,
+    seat170, setSeat170,
+    seat171, setSeat171,
+    seat172, setSeat172,
+    seat173, setSeat173,
+    seat174, setSeat174,
+    seat175, setSeat175,
+    seat176, setSeat176,
+    seat177, setSeat177,
+    seat178, setSeat178,
+    seat179, setSeat179,
+    seat180, setSeat180,
+    };
+    };
