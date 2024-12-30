06f7641d41f9d1bc0e7b58cb283ab390561f5bf5
Klaci00 <kenyolaszlo6@gmail.com>
2024-12-30 13:13:14 +0100

    Fixed a slice function in FetchVenueDetails at Venue.

--- frontend/movies-front/src/Venues/HTTP/FetchVenueDetails.jsx
+++ frontend/movies-front/src/Venues/HTTP/FetchVenueDetails.jsx
@@ -4,7 +4,8 @@ export const FetchVenueDetails_new = async(BASE_URL, venue, setVenue, venueId, s
       const getVenues = await apiClient.get(`${BASE_URL}venues/${venueId}`)
             .then(response => {
                   setVenue(response.data);
-                  seats.slice(0,response.data.room_style.capacity).forEach((seatItem, index) => {
+                  console.log('response.data.room_style.capacity',response.data.room_style[0].capacity);
+                  seats.slice(0,response.data.room_style[0].capacity).forEach((seatItem, index) => {
                         const seatKey = `seat_${String(index).padStart(3, '0')}`;
                         if (response.data.seats[seatKey] !== undefined) {
                               seatItem.setSeat(response.data.seats[seatKey]==true?2:1);
