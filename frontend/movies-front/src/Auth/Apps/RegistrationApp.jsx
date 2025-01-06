48e6ba417ea13dfda688604cef4a891aaf52ad1d
Klaci00 <kenyolaszlo6@gmail.com>
2025-01-06 13:04:52 +0100

    Registration css fix.

--- frontend/movies-front/src/Auth/Apps/RegistrationApp.jsx
+++ frontend/movies-front/src/Auth/Apps/RegistrationApp.jsx
@@ -1,72 +1,73 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './RegistrationApp.module.css'; // Import the CSS module
 
 export const RegistrationApp = (props) => {
     return (
-        <form onSubmit={props.handleRegister}>
-            <h2>Register</h2>
-            <div>
-                <input
-                    type='text'
-                    placeholder='Keresztnév'
-                    value={props.firstname}
-                    onChange={(e) => props.setFirstname(e.target.value)}
-                />
-                {props.problem.includes('A keresztnév és a vezetéknév megadása kötelező!') && <span style={{color:'red'}}>A keresztnév és a vezetéknév megadása kötelező!</span>}
-            </div>
-            <div>
-                <input
-                    type='text'
-                    placeholder='Vezetéknév'
-                    value={props.lastname}
-                    onChange={(e) => props.setLastname(e.target.value)}
-                />
-                {props.problem.includes('A keresztnév és a vezetéknév megadása kötelező!') && <span style={{color:'red'}}>A keresztnév és a vezetéknév megadása kötelező!</span>}
-            </div>
-            <div>
-                <input
-                    type='text'
-                    placeholder='Felhasználónév'
-                    value={props.username}
-                    onChange={(e) => props.setUsername(e.target.value)}
-                />
-                {props.problem.includes('A felhasználónévnek legalább 5 karakter hosszúnak kell lennie!') && <span style={{color:'red'}}>A felhasználónévnek legalább 5 karakter hosszúnak kell lennie!</span>}
-                {props.problem.includes('A felhasználónév nem tartalmazhat szóközt!') && <span style={{color:'red'}}>A felhasználónév nem tartalmazhat szóközt!</span>}
-            </div>
-            <div>
-                <input
-                    type='text'
-                    placeholder='E-mail'
-                    value={props.email}
-                    onChange={(e) => props.setEmail(e.target.value)}
-                />
-                {props.problem.includes('Az email cím formátuma nem megfelelő!') && <span style={{color:'red'}}>Az email cím formátuma nem megfelelő!</span>}
-            </div>
-            <div>
-                <input
-                    type='password'
-                    placeholder='Jelszó'
-                    value={props.password}
-                    onChange={(e) => props.setPassword(e.target.value)}
-                />
-                {props.problem.includes('A jelszónak legalább 8 karakter hosszúnak kell lennie!') && <span style={{color:'red'}}>A jelszónak legalább 8 karakter hosszúnak kell lennie!</span>}
-                {props.problem.includes('A jelszónak tartalmaznia kell legalább egy nagybetűt, egy kisbetűt, egy számjegyet és egy speciális karaktert!') && <span style={{color:'red'}}>A jelszónak tartalmaznia kell legalább egy nagybetűt, egy kisbetűt, egy számjegyet és egy speciális karaktert!</span>}
-            </div>
-            <div>
-                <input
-                    type='password'
-                    placeholder='Jelszó megismétlése'
-                    value={props.confPW}
-                    onChange={(e) => props.setConfPW(e.target.value)}
-                />
-                {props.problem.includes('A jelszavak nem egyeznek!') && <span style={{color:'red'}}>A jelszavak nem egyeznek!</span>}
-            </div>
-            <button type='submit' disabled={props.buttonDisabled}>Register</button>
-            {props.error && <span style={{color:'red'}}>{props.error}</span>}
-            <span style={{color:'red'}}>{props.problem}</span>
-        </form>
+        <div className={styles.container}>
+            <form onSubmit={props.handleRegister}>
+                <h2>Regisztráció</h2>
+                <div>
+                    <input
+                        type='text'
+                        placeholder='Keresztnév'
+                        value={props.firstname}
+                        onChange={(e) => props.setFirstname(e.target.value)}
+                    />
+                    {props.problem.includes('A keresztnév és a vezetéknév megadása kötelező!') && <span>A keresztnév és a vezetéknév megadása kötelező!</span>}
+                </div>
+                <div>
+                    <input
+                        type='text'
+                        placeholder='Vezetéknév'
+                        value={props.lastname}
+                        onChange={(e) => props.setLastname(e.target.value)}
+                    />
+                    {props.problem.includes('A keresztnév és a vezetéknév megadása kötelező!') && <span>A keresztnév és a vezetéknév megadása kötelező!</span>}
+                </div>
+                <div>
+                    <input
+                        type='text'
+                        placeholder='Felhasználónév'
+                        value={props.username}
+                        onChange={(e) => props.setUsername(e.target.value)}
+                    />
+                    {props.problem.includes('A felhasználónévnek legalább 5 karakter hosszúnak kell lennie!') && <span>A felhasználónévnek legalább 5 karakter hosszúnak kell lennie!</span>}
+                    {props.problem.includes('A felhasználónév nem tartalmazhat szóközt!') && <span>A felhasználónév nem tartalmazhat szóközt!</span>}
+                </div>
+                <div>
+                    <input
+                        type='text'
+                        placeholder='E-mail'
+                        value={props.email}
+                        onChange={(e) => props.setEmail(e.target.value)}
+                    />
+                    {props.problem.includes('Az email cím formátuma nem megfelelő!') && <span>Az email cím formátuma nem megfelelő!</span>}
+                </div>
+                <div>
+                    <input
+                        type='password'
+                        placeholder='Jelszó'
+                        value={props.password}
+                        onChange={(e) => props.setPassword(e.target.value)}
+                    />
+                    {props.problem.includes('A jelszónak legalább 8 karakter hosszúnak kell lennie!') && <span>A jelszónak legalább 8 karakter hosszúnak kell lennie!</span>}
+                    {props.problem.includes('A jelszónak tartalmaznia kell legalább egy nagybetűt, egy kisbetűt, egy számjegyet és egy speciális karaktert!') && <span>A jelszónak tartalmaznia kell legalább egy nagybetűt, egy kisbetűt, egy számjegyet és egy speciális karaktert!</span>}
+                </div>
+                <div>
+                    <input
+                        type='password'
+                        placeholder='Jelszó megerősítése'
+                        value={props.confPW}
+                        onChange={(e) => props.setConfPW(e.target.value)}
+                    />
+                    {props.problem.includes('A jelszavak nem egyeznek!') && <span>A jelszavak nem egyeznek!</span>}
+                </div>
+                <button type='submit' disabled={props.buttonDisabled}>Regisztráció</button>
+            </form>
+        </div>
     );
-}
+};
 
 RegistrationApp.propTypes = {
     handleRegister: PropTypes.func.isRequired,
@@ -83,8 +84,7 @@ RegistrationApp.propTypes = {
     confPW: PropTypes.string.isRequired,
     setConfPW: PropTypes.func.isRequired,
     problem: PropTypes.string.isRequired,
-    error: PropTypes.string,
-    buttonDisabled: PropTypes.bool.isRequired
+    buttonDisabled: PropTypes.bool.isRequired,
 };
 
 export default RegistrationApp;
