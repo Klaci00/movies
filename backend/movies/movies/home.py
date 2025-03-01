b0a6d8cde09f9d4ae4462209b2b14413d1ae2e1f
Klaci00 <kenyolaszlo6@gmail.com>
2025-03-01 18:45:35 +0100

    Removed static files from django as they became obsolete.

--- backend/movies/movies/home.py
+++ backend/movies/movies/home.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 def home(request):
-    return render(request,'index.html')
+    return HttpResponseRedirect('https://localhost:7260/index.html')
