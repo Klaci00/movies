961c949b931ea15ae3b6749d602004426cdc8e54
Klaci00 <kenyolaszlo6@gmail.com>
2025-03-02 16:53:00 +0100

    Footer improvement.

--- frontend/movies-front/src/Layout/Apps/FooterApp.jsx
+++ frontend/movies-front/src/Layout/Apps/FooterApp.jsx
@@ -1,5 +1,6 @@
-// Footer.js
-
+import { FaInstagram,FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 import React from 'react';
 import styles from './FooterApp.module.css'; // Import the CSS module
 
@@ -7,27 +8,25 @@ const FooterApp = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.footerContainer}>
-                <div className={styles.footerSection}>
+
                     <h2 className={styles.heading}>About Us</h2>
                     <p>We're passionate about providing the best service. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.</p>
-                </div>
-                <div className={styles.footerSection}>
+
                     <h2 className={styles.heading}>Quick Links</h2>
-                    <ul className={styles.ul}>
-                        <li className={styles.li}><a href="#" className={styles.a}>Home</a></li>
-                        <li className={styles.li}><a href="#" className={styles.a}>Services</a></li>
-                        <li className={styles.li}><a href="#" className={styles.a}>Contact</a></li>
-                        <li className={styles.li}><a href="#" className={styles.a}>Blog</a></li>
-                    </ul>
-                </div>
-                <div className={styles.footerSection}>
+                    <div className={styles.quickLinks}>
+                        <a href="#" className={styles.a}>Home</a>
+                        <a href="#" className={styles.a}>Services</a>
+                        <a href="#" className={styles.a}>Contact</a>
+                        <a href="#" className={styles.a}>Blog</a>
+                    </div>
+
+
                     <h2 className={styles.heading}>Follow Us</h2>
                     <div className={styles.socialLinks}>
-                        <a href="#" className={styles.icon}><i className="fab fa-facebook"></i></a>
-                        <a href="#" className={styles.icon}><i className="fab fa-twitter"></i></a>
-                        <a href="#" className={styles.icon}><i className="fab fa-instagram"></i></a>
-                        <a href="#" className={styles.icon}><i className="fab fa-linkedin"></i></a>
-                    </div>
+                        <Link   target="blank" to={"https://instagram.com"}><FaInstagram id={styles.insta} /></Link>
+                        <Link to={"https://facebook.com"}><FaFacebook /></Link>
+                        <Link to={"https://twitter.com"}><FaXTwitter /></Link>
+
                 </div>
             </div>
             <div className={styles.footerBottom}>
